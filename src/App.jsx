42aa02558271beb0bdc11d41e1b6924da77c9b88
BlancSofia27 <blancsofia27@gmail.com>
2024-08-20 22:59:18 -0300

    add last version

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import React from "react"
+import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-} from "react-router-dom"
-import Landing from "./views/Landing"
-import BarberShop from "./views/BarberShop"
-import Consulting from "./views/Consulting"
-import Tourism from "./views/Tourism"
+} from "react-router-dom";
+import Landing from "./views/Landing";
+import BarberShop from "./views/BarberShop";
+import Consulting from "./views/Consulting";
+import Tourism from "./views/Tourism";
+
 function App() {
   return (
-    <Router>
+    <Router basename="/Landing">
       <Routes>
-        <Route path="/" element={<Navigate to="/Landing" />} />
-        <Route path="/Landing" element={<Landing />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/BarberShop" element={<BarberShop />} />
         <Route path="/Consulting" element={<Consulting />} />
         <Route path="/Tourism" element={<Tourism />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
+
